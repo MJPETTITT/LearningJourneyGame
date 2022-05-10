cadd4d5028583f862db4c6629c75e8c2853749dc
MJPETTITT <105298876+MJPETTITT@users.noreply.github.com>
2022-05-10 17:01:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -74,9 +74,39 @@ function flipOnOff() {
 		flip();
 		populate();
 		populateNumber();
+		startTimer();
 	} else {
 		unflip();
 		depopulate()
 		progress();
+		stopTimer();
 	}
+}
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0){
+    return
+  }
+  
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  console.log(m)
+  timer5 = setTimeout(startTimer, 1000);
+  
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+
+function stopTimer() {
+	 clearTimeout(timer5);
+	document.getElementById('timer').innerHTML = "5:00";
 }
